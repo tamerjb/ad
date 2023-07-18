d4440a60131ce8717b8d448c539aebced07f2980
Tamer <tamerjb.98@gmail.com>
2023-07-18 22:21:28 +0300

    added file structure

---
+++ advice/src/components/App/App.js
@@ -0,0 +1,14 @@
+import logo from '../../images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img />
+      </header>
+    </div>
+  );
+}
+
+export default App;
